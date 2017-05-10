5b8869e7159f660d91a881894ea0da2a770c5cdf
Jeffrey Pessutti <JPessutti@NOAAs-iMac.local>
2017-05-09 21:17:45 -0400

    Error handling on sign in done

--- MesaMesa/ViewController.swift
+++ MesaMesa/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-            errorText.isHidden = true
+        errorText.isHidden = true
     }
     
     
@@ -29,17 +29,50 @@ class ViewController: UIViewController {
         FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: pwordTextField.text!, completion: { (user, error) in
             print("We tried to sign in")
             if error != nil {
-                self.errorText.isHidden=false
-                print("Hey, Error: \(error)")
-                                FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.pwordTextField.text!, completion: { (user, error) in
+                
+                
+                // an error occurred while attempting login
+                if let errCode = FIRAuthErrorCode(rawValue: (error?._code)!) {
+                    switch errCode {
+                    case .errorCodeEmailAlreadyInUse:
+                        print("case1")
+                    case .errorCodeInvalidEmail:
+                        print("case2 invalid email")
+                    case .errorCodeWrongPassword:
+                        print("case3 wrong password")
+                        self.errorText.isHidden=false
+                        //let delayInSeconds = 3.0
+                        //DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delayInSeconds) {
+                            // here code perfomed with delay
+                        //}
+                        
+                    default : print("fu")
+                    }
+                }
+                
+                
+                //print("Hey, Error: \(error)")
+                FIRAuth.auth()?.createUser(withEmail: self.emailTextField.text!, password: self.pwordTextField.text!, completion: { (user, error) in
+                    
                     print("we tried to create a user")
                     if error != nil {
-                       print("Hey, Error: \(error)")
+                        print("Hey, Error: \(error!)")
+                        //self.errorText.isHidden = true
                     }else{
                         print("New User Created")
+                        self.errorText.text="We just created a MesaMesa account for you. Welcome to the table."
+                        self.errorText.textColor = UIColor.black
+                        self.errorText.isHidden = false
+                        let delayInSeconds = 2.5
+                        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delayInSeconds) {
+                            // here code perfomed with delay
+                        }
+
                     }
                 })
             }else{
+                self.errorText.isHidden = true
+
                 print("Signed-in Successfully")
             }
         })
